beeaf69d23e84d87142c9f0ac4329593fea6f9d0
Renduchinthala Sai praneeth kumar <2000030859@kluniversity.in>
2022-09-14 02:04:13 +0530

    initial

---
+++ Irisproject/varna/varnaapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VarnaappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'varnaapp'
---
+++ Irisproject/varna/varnaapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import pandas as pd
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return render(request, 'home.html')
+# def result(request):
+#     data={"dataset":PredResults.objects.all}
+#     return render(request, 'result.html',data)
+def predect(request):
+    if request.method == "POST":
+        sepal_length =request.POST.get('sepal_length')
+        sepal_width = request.POST.get('sepa_width')
+        petal_length = (request.POST.get('petal_length'))
+        petal_width = (request.POST.get('petal_width'))
+        print(sepal_length)
+        # sepal_length = float(input("Enter sepal_length: "))
+        # sepal_width = float(input("Enter sepa_width: "))
+        # petal_length = float(input("Enter petal_length: "))
+        # petal_width = float(input("Enter petal_width: "))
+
+        model=pd.read_pickle(r"C:\Users\Sai Praneeth\OneDrive\Desktop\kog test\varna\varnaapp\new_model.pickle")
+        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+        print(result[0])
+        classification =result[0]
+
+        return render(request,'result.html',{'result':classification})
+
+
+
